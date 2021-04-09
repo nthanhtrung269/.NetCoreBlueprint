2d552b58424aeef0da17657af789f540801af7a1
nthanhtrung269 <nthanhtrung269@gmail.com>
2021-04-09 14:59:27 +0700

    master | Add Blueprint.Mapster

---
+++ Blueprint.Mapster/WeatherForecastDataModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Blueprint.Mapster
+{
+    public class WeatherForecastDataModel
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
