293057df11de2a0208d3c2d708d625b77e56e4e7
nthanhtrung269 <nthanhtrung269@gmail.com>
2021-04-09 15:06:11 +0700

    master | Add Blueprint.Nlog

---
+++ Blueprint.Nlog/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+
+namespace Blueprint.Nlog
+{
+    public class Startup
+    {
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// The Startup constructor.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="environment">The environment.</param>
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
+        {
+            // Gets the factory for ILogger instances.
+            var nlogLoggerProvider = new NLogLoggerProvider();
+            // Creates an ILogger.
+            _logger = nlogLoggerProvider.CreateLogger(typeof(Startup).FullName);
+
+            // Gets environment in the web.config file https://weblog.west-wind.com/posts/2020/Jan/14/ASPNET-Core-IIS-InProcess-Hosting-Issues-in-NET-Core-31
+            string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            _logger.LogInformation($"Environment name: {environmentName}");
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(environment.ContentRootPath)
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environmentName ?? "Production"}.json", optional: true);
+
+            // Sets the new Configuration
+            configuration = builder.Build();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddLogging(
+               builder =>
+               {
+                   builder.AddFilter("Microsoft", LogLevel.Warning)
+                          .AddFilter("System", LogLevel.Warning)
+                          .AddFilter("NToastNotify", LogLevel.Warning)
+                          .AddConsole();
+               });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
