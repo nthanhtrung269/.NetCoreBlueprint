ee9ee69f5b80537261be9e628447007edd967ec6
nthanhtrung269 <nthanhtrung269@gmail.com>
2021-04-09 14:53:32 +0700

    master | Add Blueprint.HttpClient.IntegrationTestings

---
+++ Blueprint.HttpClient.IntegrationTestings/BusinessServices/Provider1Api/Provider1ApiServiceIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using Blueprint.HttpClient1;
+using Blueprint.HttpClient1.BusinessServices.Provider1Api;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blueprint.HttpClient.IntegrationTestings.BusinessServices.Provider1Api
+{
+    /// <summary>
+    /// Document: Integration tests https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-3.1
+    /// </summary>
+    public class Provider1ApiServiceIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public Provider1ApiServiceIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAuthorizationInfo_ShouldRunCorrectly()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                // Arrange
+                var Provider1ApiService = scope.ServiceProvider.GetRequiredService<IProvider1ApiService>();
+
+                // Act
+                Provider1AuthorizationResponse response = await Provider1ApiService.GetAuthorizationInfo();
+
+                // Assert
+                Assert.NotNull(response);
+                Assert.NotNull(response.Token);
+                Assert.NotNull(response.UserId);
+                Assert.NotNull(response.Secret);
+                Assert.NotNull(response.AdSessionGuid);
+            }
+        }
+    }
+}
---
+++ Blueprint.HttpClient.IntegrationTestings/CustomWebApplicationFactory.cs
@@ -0,0 +1,41 @@
+﻿using Blueprint.HttpClient1.DataServices;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace Blueprint.HttpClient.IntegrationTestings
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DBContext>));
+
+                services.Remove(dbContextDescriptor);
+
+                services.AddDbContext<DBContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<DBContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    logger.LogInformation("DBContext, ReportContext using in memory database...");
+                }
+            });
+        }
+    }
+}
